08d1846d0995ee6fa93fbe87d742fea4fd3933b2
Daniel Kingery <danielkingery0@gmail.com>
2025-05-17 18:59:00 -0400

    Replace xampp with lamp in install script

---
+++ install/lamp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MARIA_DB_ROOT_PASSWORD="Password"
+
+# Set up the LAMP stack on a Raspberry Pi
+sudo apt update
+
+# Install packages
+sudo apt install -y apache2 php libapache2-mod-php mariadb-server php-mysql phpmyadmin
+
+
+# Set up Apache
+sudo ufw allow in "Apache Full"
+sudo chmod -R 0755 /var/www/html/
+sudo systemctl enable apache2 # Start Apache on boot
+sudo systemctl start apache2
+
+# Set up MariaDB
+# sudo mysql -e "CREATE USER 'root' IDENTIFIED BY 'password';"
+# sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '$MARIA_DB_ROOT_PASSWORD';"
+
+sudo echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf
+sudo systemctl restart apache2
