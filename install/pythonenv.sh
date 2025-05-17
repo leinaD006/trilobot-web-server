a380b37702d9c381fc0247610155595cf3b5df1b
Daniel Kingery <danielkingery0@gmail.com>
2025-05-17 19:12:13 -0400

    Fix the environment name when activating

--- install/pythonenv.sh
+++ install/pythonenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set up python environment
 python3 -m venv venv
-source env/bin/activate
+source venv/bin/activate
 pip install -r $INSTALL_DIR/requirements.txt
 echo "Python environment set up successfully."
