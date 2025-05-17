ffa5f0f91065e1459475b8bbb85b19961012236a
Daniel Kingery <danielkingery0@gmail.com>
2025-05-17 19:45:33 -0400

    Remove venv log

--- install/pythonenv.sh
+++ install/pythonenv.sh
@@ -2,6 +2,5 @@
 # Set up python environment
 python3 -m venv venv
 source venv/bin/activate
-echo $VIRTUAL_ENV
 pip install -r $INSTALL_DIR/requirements.txt
 echo "Python environment set up successfully."
