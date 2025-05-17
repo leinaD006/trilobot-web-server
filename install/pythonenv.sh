f41c104865ac69ff37b9054cd0ef0e6076c6fe20
Daniel Kingery <danielkingery0@gmail.com>
2025-05-17 19:20:35 -0400

    Check virtual env during install

--- install/pythonenv.sh
+++ install/pythonenv.sh
@@ -2,5 +2,6 @@
 # Set up python environment
 python3 -m venv venv
 source venv/bin/activate
+echo $VIRTUAL_ENV
 pip install -r $INSTALL_DIR/requirements.txt
 echo "Python environment set up successfully."
