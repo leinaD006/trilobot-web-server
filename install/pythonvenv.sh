58f2d7f68ad4dbd5a57a5953cfe9c80d8afd5394
Daniel Kingery <danielkingery0@gmail.com>
2025-05-18 18:43:50 -0400

    Clean up python venv install and change name

--- install/pythonvenv.sh
+++ install/pythonvenv.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
 # Set up python environment
 python3 -m venv venv
+
+# Switch to environment
 source venv/bin/activate
+
+# Install required packages
 pip install -r $INSTALL_DIR/requirements.txt
+
 echo "Python environment set up successfully."
