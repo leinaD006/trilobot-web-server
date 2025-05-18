ccf130386d8795330b5200f3678393a0a7b3c1a5
Daniel Kingery <danielkingery0@gmail.com>
2025-05-18 15:35:41 -0400

    Lamp Install Test 1

--- install/lamp.sh
+++ install/lamp.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-MARIA_DB_ROOT_PASSWORD="Password"
+# MARIA_DB_ROOT_PASSWORD="Password"
 
 # Set up the LAMP stack on a Raspberry Pi
 sudo apt update
 
 # Install packages
-sudo apt install -y apache2 php libapache2-mod-php mariadb-server php-mysql phpmyadmin
+sudo apt install -y apache2 php libapache2-mod-php mariadb-server php-mysql
 
 
 # Set up Apache
-sudo ufw allow in "Apache Full"
-sudo chmod -R 0755 /var/www/html/
+# sudo ufw allow in "Apache Full"
+# sudo chmod -R 0755 /var/www/html/
 sudo systemctl enable apache2 # Start Apache on boot
 sudo systemctl start apache2
 
@@ -19,5 +19,9 @@ sudo systemctl start apache2
 # sudo mysql -e "CREATE USER 'root' IDENTIFIED BY 'password';"
 # sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '$MARIA_DB_ROOT_PASSWORD';"
 
-sudo echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf
+# sudo echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf
+
+sudo echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+
 sudo systemctl restart apache2
+
