b598a1ad3f0130ac064c41f4ba6b7499027779f8
Daniel Kingery <danielkingery0@gmail.com>
2025-05-18 15:58:57 -0400

    Lamp Install Test 2

--- install/lamp.sh
+++ install/lamp.sh
@@ -11,7 +11,8 @@ sudo apt install -y apache2 php libapache2-mod-php mariadb-server php-mysql
 
 # Set up Apache
 # sudo ufw allow in "Apache Full"
-# sudo chmod -R 0755 /var/www/html/
+sudo chmod -R 0755 /var/www/html/
+sudo chown -R $USER:$USER /var/www/html/
 sudo systemctl enable apache2 # Start Apache on boot
 sudo systemctl start apache2
 
