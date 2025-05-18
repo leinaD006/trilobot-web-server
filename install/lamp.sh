3b7f9fdaa3e189a0d0fafb3dfa1bc4abf5230bf4
Daniel Kingery <danielkingery0@gmail.com>
2025-05-18 18:41:38 -0400

    Clean up LAMP script

--- install/lamp.sh
+++ install/lamp.sh
@@ -8,7 +8,6 @@ sudo apt update
 # Install packages
 sudo apt install -y apache2 php libapache2-mod-php mariadb-server php-mysql
 
-
 # Set up Apache
 # sudo ufw allow in "Apache Full"
 sudo chmod -R 0755 /var/www/html/
@@ -16,11 +15,12 @@ sudo chown -R $USER:$USER /var/www/html/
 sudo systemctl enable apache2 # Start Apache on boot
 sudo systemctl start apache2
 
+# Set up PHPMyAdmin
 sudo apt install phpmyadmin -y
-
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIA_DB_ROOT_PASSWORD';"
 
-sudo echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+# sudo echo "<?php phpinfo(); ?>" > /var/www/html/info.php
 
+# Final restart
 sudo systemctl restart apache2
 
