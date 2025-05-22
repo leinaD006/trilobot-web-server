dfa772de967d529137b6c207920a5838fbbb20c4
Daniel Kingery <danielkingery0@gmail.com>
2025-05-22 17:13:41 -0400

    Run python from php test 1

--- web/trilobot/index.php
+++ web/trilobot/index.php
@@ -3,6 +3,8 @@
 $python = "/var/www/html/python/venv/bin/python";
 $script_path = "/var/www/html/python/scripts";
 
+echo "$python $script_path/examples/flash_underlights.py\n";
+
 $output = shell_exec("$python $script_path/examples/flash_underlights.py");
 echo $output;
 ?>
