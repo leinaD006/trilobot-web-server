4688b2705135331e6c1c560478b35faef31f2f55
Daniel Kingery <danielkingery0@gmail.com>
2025-05-22 17:27:49 -0400

    Add apache permission to use gpio

--- install/lamp.sh
+++ install/lamp.sh
@@ -20,6 +20,9 @@ sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIA_DB_ROOT_PASSW
 # Remove default index.html
 sudo rm /var/www/html/index.html
 
+# Give apache user permission to gpio
+sudo usermod -a -G gpio www-data
+
 # Final restart
 sudo systemctl restart apache2
 
