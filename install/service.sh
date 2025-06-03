0d7adad888a1b3e40c5922ce24dc58e486abba43
Daniel Kingery <danielkingery0@gmail.com>
2025-06-03 17:11:35 -0400

    Add ipaddress service

---
+++ install/service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Set up the ipaddress service
+
+sudo cp $INSTALL_DIR/ipaddress.service /etc/systemd/system/ipaddress.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable mypythonscript.service
+
