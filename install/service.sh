673ee3da6d0da887853a588ea5d30a1c698aff01
Daniel Kingery <danielkingery0@gmail.com>
2025-06-03 17:23:56 -0400

    Create working dir for ipaddress service

--- install/service.sh
+++ install/service.sh
@@ -3,6 +3,8 @@ set -e
 
 # Set up the ipaddress service
 
+sudo mkdir -p /opt/ipaddress
+
 sudo cp $INSTALL_DIR/ipaddress.service /etc/systemd/system/ipaddress.service
 
 sudo systemctl daemon-reload
