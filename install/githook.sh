58b14ccca33967656010583a4aa68b8c54a2e52b
Daniel Kingery <danielkingery0@gmail.com>
2025-06-03 17:45:18 -0400

    Add git hook install

---
+++ install/githook.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Add post-merge hook to .git/hooks
+$HOOK_DIR="$SCRIPT_DIR/.git/hooks"
+if [ ! -d "$HOOK_DIR" ]; then
+    mkdir -p "$HOOK_DIR"
+fi
+cp "$SCRIPT_DIR/install/files/post-merge" "$HOOK_DIR/post-merge"
+chmod +x "$HOOK_DIR/post-merge"
+echo "Post-merge hook installed successfully at $HOOK_DIR/post-merge."
