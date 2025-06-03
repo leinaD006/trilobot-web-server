617a12f47c1ee1cab44330e4e63b8abdc5e79e74
Daniel Kingery <danielkingery0@gmail.com>
2025-06-03 16:24:34 -0400

    Fix import time

--- python/scripts/core/ipaddress.py
+++ python/scripts/core/ipaddress.py
@@ -1,5 +1,7 @@
 import netifaces
+import time
 from trilobot import *
+
 light_groups = {
     "1" : (LIGHT_MIDDLE_LEFT),
     "2" : (LIGHT_FRONT_LEFT),
@@ -40,6 +42,6 @@ print(f"WiFi IP Address: {wifi_ip}")
 for c in wifi_ip:
     print(light_groups["0"])
     tbot.set_underlights(light_groups["0"], RED)
-    sleep(1)
+    time.sleep(1)
     tbot.clear_underlights(light_groups["0"])
-    sleep(0.2)
+    time.sleep(0.2)
