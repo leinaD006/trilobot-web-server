cc533aa535db4d6cd5b27877a4bfc01a54bf76f6
Daniel Kingery <danielkingery0@gmail.com>
2025-06-04 14:04:21 -0400

    Add script for creating a symlink folder to work in

---
+++ install/folder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Creates a symlinked folder for students to work in
+if [ ! -d "$HOME/trilobot/python"]; then
+    mkdir -p "$HOME/trilobot/python"
+    ln -s "$HOME/trilobot/python" "$WEB_ROOT/python/scripts/user"
+fi
+
+echo "Created symlink for student scripts in $HOME/trilobot/python"
