a3352f762e99c3972642c7077c471b345eb11f62
Daniel Kingery <danielkingery0@gmail.com>
2025-06-05 15:24:06 -0400

    Add msa301 examples

--- install/examples.sh
+++ install/examples.sh
@@ -12,13 +12,17 @@ fi
 git clone https://github.com/pimoroni/trilobot-python "$INSTALL_DIR/trilobot-python"
 
 # Copy example files to the examples directory
-cp -r "$INSTALL_DIR/trilobot-python/examples"/* "$EXAMPLE_DIR/"
+cp -r "$INSTALL_DIR/trilobot-python/examples"/* "$EXAMPLE_DIR/trilobot/"
 
 # Delete the cloned repository
 rm -rf "$INSTALL_DIR/trilobot-python"
 
 git clone https://github.com/pimoroni/bme680-python "$INSTALL_DIR/bme680-python"
-cp -r "$INSTALL_DIR/bme680-python/examples"/* "$EXAMPLE_DIR/"
+cp -r "$INSTALL_DIR/bme680-python/examples"/* "$EXAMPLE_DIR/bme680/"
 rm -rf "$INSTALL_DIR/bme680-python"
+
+git clone https://github.com/pimoroni/msa301-python "$INSTALL_DIR/msa301-python"
+cp -r "$INSTALL_DIR/msa301-python/examples"/* "$EXAMPLE_DIR/msa301"
+rm -rf "$INSTALL_DIR/msa301-python"
 
 echo "Example files installed successfully in $EXAMPLE_DIR"
