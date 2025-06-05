ac5c41d084b2e2165cf1a754430e602df67d47df
Daniel Kingery <danielkingery0@gmail.com>
2025-06-05 15:25:33 -0400

    Fix create the required folders

--- install/examples.sh
+++ install/examples.sh
@@ -8,6 +8,10 @@ if [ ! -d "$EXAMPLE_DIR" ]; then
     mkdir -p "$EXAMPLE_DIR"
 fi
 
+if [ ! -d "$EXAMPLE_DIR" ]; then
+    mkdir -p "$EXAMPLE_DIR/trilobot"
+fi
+
 # Installs example python files for Trilobot
 git clone https://github.com/pimoroni/trilobot-python "$INSTALL_DIR/trilobot-python"
 
@@ -17,9 +21,17 @@ cp -r "$INSTALL_DIR/trilobot-python/examples"/* "$EXAMPLE_DIR/trilobot/"
 # Delete the cloned repository
 rm -rf "$INSTALL_DIR/trilobot-python"
 
+if [ ! -d "$EXAMPLE_DIR" ]; then
+    mkdir -p "$EXAMPLE_DIR/bme680"
+fi
+
 git clone https://github.com/pimoroni/bme680-python "$INSTALL_DIR/bme680-python"
 cp -r "$INSTALL_DIR/bme680-python/examples"/* "$EXAMPLE_DIR/bme680/"
 rm -rf "$INSTALL_DIR/bme680-python"
+
+if [ ! -d "$EXAMPLE_DIR" ]; then
+    mkdir -p "$EXAMPLE_DIR/mas301"
+fi
 
 git clone https://github.com/pimoroni/msa301-python "$INSTALL_DIR/msa301-python"
 cp -r "$INSTALL_DIR/msa301-python/examples"/* "$EXAMPLE_DIR/msa301"
