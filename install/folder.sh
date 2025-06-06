e4cac7d9e3535891190ad03642bcfe1b57667119
Daniel Kingery <danielkingery0@gmail.com>
2025-06-06 15:14:50 -0400

    Fix symlink folder creation

--- install/folder.sh
+++ install/folder.sh
@@ -4,6 +4,13 @@ set -e
 # Creates a symlinked folder for students to work in
 if [ ! -d "$HOME/trilobot/python" ]; then
     mkdir -p "$HOME/trilobot/python"
+fi
+
+if [ ! -d "$WEB_ROOT/python/scripts" ]; then
+        mkdir -p "$WEB_ROOT/python/scripts"
+fi
+
+if [ ! -L "$WEB_ROOT/python/scripts/user" ]; then
     ln -s "$HOME/trilobot/python" "$WEB_ROOT/python/scripts/user"
 fi
 
